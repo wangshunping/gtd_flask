a4fa24db52c52a191f613a87be98350479863da3
wangshunping <lionelwang93@163.com>
2015-06-30 20:20:12 +0800

    init

---
+++ app/tests/test_basics.py
@@ -0,0 +1,10 @@
+import unittest
+from flask import current_app
+from app import create_app, db
+
+class BasicTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
